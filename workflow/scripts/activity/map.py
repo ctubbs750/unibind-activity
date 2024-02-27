55e810992837565678199d6fde49f387401fdb8a
Colby Tubbs <ctubbs750@gmail.com>
2024-02-27 15:16:53 -0600

    snake

--- workflow/scripts/activity/map.py
+++ workflow/scripts/activity/map.py
@@ -1,7 +1,8 @@
 """"""
 
-import re
-from sklearn import metrics
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 from pandas import DataFrame, read_csv, merge
 from statsmodels.stats.proportion import proportion_confint
 
@@ -44,29 +45,22 @@ def main() -> None:
     # Merge with pvals
     sites = merge(sites, pvals, on="score", how="left")
 
-    # Try summary
-    try:
-        # Get count summary info
-        sites[[0, 1, 2, 3]] = (
-            sites["summary"]
-            .str.replace(",", ":")
-            .str.split(":", expand=True)
-            .fillna(0)
-            .astype(int)
-        )
-        # Whenever value in col 0 is 1, then switch cols 0,1 with 2,3
-        sites.loc[sites[0] == 1, [0, 1, 2, 3]] = sites.loc[
-            sites[0] == 1, [2, 3, 0, 1]
-        ].values
+    # Read score and summary and make dict of (un)bound:count
+    summary_dict = {
+        score: dict([i.split(":") for i in summary.split(",")])
+        for score, summary in zip(sites["score"], sites["summary"])
+    }
 
-        # Drop and rename
-        sites = sites.drop(columns=[0, 2]).rename(
-            columns={1: "n_unbound", 3: "n_bound"}
-        )
-    # This is when there is no bound - not exactly true below but quick fix for now #NOTE
-    except ValueError:
-        sites["n_unbound"] = 0
-        sites["n_bound"] = 0
+    # Add '0' or '1' to each key if not present
+    for key in summary_dict.keys():
+        if "0" not in summary_dict[key].keys():
+            summary_dict[key]["0"] = 0
+        if "1" not in summary_dict[key].keys():
+            summary_dict[key]["1"] = 0
+
+    # Add n_unbound and n_bound to sites
+    sites["n_unbound"] = [int(summary_dict[s]["0"]) for s in sites["score"]]
+    sites["n_bound"] = [int(summary_dict[s]["1"]) for s in sites["score"]]
 
     # Total motifs
     sites["n_motifs"] = sites["n_bound"] + sites["n_unbound"]
@@ -74,71 +68,37 @@ def main() -> None:
     # Proportion bound - Activity
     sites["activity"] = sites["n_bound"] / sites["n_motifs"]
 
-    # Add standard error on proportion
+    # Add standard error on proportion - wilson interval to help with small proportions
     sites["ci95_lbound"], sites["ci95_rbound"] = proportion_confint(
-        sites["n_bound"], sites["n_motifs"]
+        sites["n_bound"], sites["n_motifs"], method="wilson"
     )
 
+    # Width of CI
+    sites["ci_width"] = sites["ci95_rbound"] - sites["ci95_lbound"]
+
     # Rolling motif count
-    sites["nmotif_roll"] = sites["n_motifs"].rolling(window=WINDOW).mean()
+    sites["ci_width_roll"] = sites["ci_width"].rolling(window=WINDOW).mean()
 
-    # Try, exept index error means motif count is good across the board, so can skip the process
-    try:
-        last_index = sites[sites["nmotif_roll"] < THRESH].index[0] - WINDOW
+    # This may be driven by small proportions of 1 - weightign by CI width, Fill with min - these are missed in the rolling
+    weights = sites["ci_width_roll"].fillna(sites["ci_width_roll"].min())
 
-        # handle negatives
-        if last_index < 0:
-            last_index = 0
+    # Train the linear regression model with weights
+    X = np.array(sites["score"]).reshape(-1, 1)
+    y = sites["activity"]
+    model = LinearRegression().fit(X, y, sample_weight=weights)
 
-        # Update to values at the cap index
-        columns_to_update = [
-            "n_unbound",
-            "n_bound",
-            "n_motifs",
-            "activity",
-            "ci95_lbound",
-            "ci95_rbound",
-            "nmotif_roll",
-        ]
+    # Get the predicted values
+    y_pred = model.predict(X)
 
-        # Truncate
-        for column in columns_to_update:
-            sites.loc[last_index:, column] = sites.loc[last_index, column]
+    # Calculate the Mean Squared Error
+    mse = mean_squared_error(y, y_pred)
 
-        # Flat that you adjusted
-        sites["adjusted"] = [1 if x > last_index else 0 for x in sites.index]
+    # Write to file
+    sites.to_csv(OUTPUT, sep="\t", index=False)
 
-        # Bound to 0 and 1
-        x = sites["perc"] / 10
-        y = sites["activity"]
-
-        # Get AUC
-        auc = metrics.auc(x, y)
-
-        # Write to file
-        sites.to_csv(OUTPUT, sep="\t", index=False)
-
-        # Save auc
-        with open(OUTPUT.replace(".tsv", ".auc"), "w") as f:
-            f.write(str(auc))
-
-    except IndexError:
-        # Flag not adjusted
-        sites["adjusted"] = 0
-
-        # Bound to 0 and 1
-        x = sites["perc"] / 10
-        y = sites["activity"]
-
-        # Get AUC
-        auc = metrics.auc(x, y)
-
-        # Write to file
-        sites.to_csv(OUTPUT, sep="\t", index=False)
-
-        # Save auc
-        with open(OUTPUT.replace(".tsv", ".auc"), "w") as f:
-            f.write(str(auc))
+    # Save auc
+    with open(OUTPUT.replace(".tsv", ".mse"), "w") as f:
+        f.write(str(mse))
 
 
 # ------------- #
